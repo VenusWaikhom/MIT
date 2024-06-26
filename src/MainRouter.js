4b8ed0ad00c375812785f548ab2955e1008d6777
Venus Waikhom <apocalyse7@gmail.com>
2024-06-26 17:57:48 +0530

    an error is there

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Users/Home/Home";
-import Developers from "./Users/Developers/Developers";
+import Developer from "./Users/Developer/Developer";
 
 function MainRouter() {
   return (
     <Routes>
       <Route path="/">
         <Route index element={<Home />} />
-        <Route path="Developers" element={<Developers />} />
+        <Route path="Developers" element={<Developer />} />
       </Route>
     </Routes>
   );
